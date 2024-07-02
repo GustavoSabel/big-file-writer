545139c27d77cf54708d31531722cf8e7a27b7e9
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2024-07-02 08:09:36 -0300

    Renomeando variáveis

--- src/problema/index.ts
+++ src/problema/index.ts
@@ -1,12 +1,12 @@
 import fs from "node:fs";
 import { Repository } from "../repository";
 
-const data = Repository.getBigArray();
+const bigArray = Repository.getBigArray();
 
 const file = await fs.promises.open("output.txt", "w");
 
 const streamWriter = file.createWriteStream({ encoding: "utf-8" });
 
-for (const item of data) {
-  streamWriter.write(item);
+for (const line of bigArray) {
+  streamWriter.write(line);
 }
--- src/solucao/index.ts
+++ src/solucao/index.ts
@@ -1,15 +1,15 @@
 import fs from "node:fs";
 import { Repository } from "../repository";
 
-const data = Repository.getBigArray();
+const bigArray = Repository.getBigArray();
 
 const file = await fs.promises.open("output.txt", "w");
 
 const streamWriter = file.createWriteStream({ encoding: "utf-8" });
 
 let pendentLines = 0;
-for (const item of data) {
-  streamWriter.write(item);
+for (const line of bigArray) {
+  streamWriter.write(line);
   if (pendentLines++ === 100_000) {
     pendentLines = 0;
     console.log(`Writing lines`);
