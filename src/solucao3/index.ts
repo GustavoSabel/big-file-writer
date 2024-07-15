62dc690f6861fe9d1b31cbdd99ae2dda3636dbeb
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2024-07-15 11:23:32 -0300

    Criada solução 3 com cork

---
+++ src/solucao3/index.ts
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+import { Repository } from "../repository";
+
+const bigArray = Repository.getBigArray();
+
+const file = await fs.promises.open("output-solucao3.txt", "w");
+
+const highWaterMark = 1024 * 1024; // 1MB
+const streamWriter = file.createWriteStream({ encoding: "utf-8", highWaterMark });
+
+streamWriter.cork()
+for (const line of bigArray) {
+  streamWriter.write(line);
+  if (streamWriter.writableNeedDrain) {
+    streamWriter.uncork()
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    streamWriter.cork()
+    console.log(`Writed ${(streamWriter.bytesWritten / 1024 / 1024).toFixed(5)} MB`);
+  }
+}
+
+streamWriter.uncork()
+streamWriter.end()
+await new Promise((resolve) => setTimeout(resolve, 100));
+console.log(`Writed ${(streamWriter.bytesWritten / 1024 / 1024).toFixed(5)} MB`);
